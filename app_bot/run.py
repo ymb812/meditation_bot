2d36609b5daf13afbc814928b194401c790be10e
Kotov Ilya <the3k27@gmail.com>
2024-05-30 12:20:18 +0300

    Handle errors for old dialog

--- app_bot/run.py
+++ app_bot/run.py
@@ -1,14 +1,25 @@
 import asyncio
+import logging
 import core.middlewares
-from aiogram import Bot, Dispatcher
-from aiogram_dialog import setup_dialogs
+from aiogram import Bot, Dispatcher, filters
+from aiogram_dialog import setup_dialogs, DialogManager, StartMode, ShowMode
+from aiogram_dialog.api.exceptions import UnknownIntent, UnknownState
 from settings import settings
 from setup import register
 from core.handlers import routers
 from core.dialogs import dialogues
+from core.states.main_menu import MainMenuStateGroup
 from aiogram.fsm.storage.redis import RedisStorage, DefaultKeyBuilder
 from scheduler import scheduler
 
+logger = logging.getLogger(__name__)
+
+
+async def handle_unknown_intent_or_state(event, dialog_manager: DialogManager):
+    logger.error('Restarting dialog: %s', event.exception)
+    await dialog_manager.start(
+        MainMenuStateGroup.main_menu, mode=StartMode.RESET_STACK, show_mode=ShowMode.DELETE_AND_SEND,
+    )
 
 bot = Bot(settings.bot_token.get_secret_value(), parse_mode='HTML')
 
@@ -19,6 +30,12 @@ storage = RedisStorage.from_url(
 dp = Dispatcher(storage=storage)
 core.middlewares.i18n.setup(dp)
 setup_dialogs(dp)
+
+# handle errors for old dialog
+dp.errors.register(
+    handle_unknown_intent_or_state,
+    filters.ExceptionTypeFilter(UnknownIntent, UnknownState),
+)
 
 for _r in routers + dialogues:
     dp.include_router(_r)
